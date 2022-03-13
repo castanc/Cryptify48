f5fc03e89f489ad8491f6b220ca86d118b228bfc
Cesar Castano <castanc@gmail.com>
2022-03-12 21:31:52 -0500

    xxx 1

---
+++ Criptify2/Cryptico48_Deploy/1_ToMinify/keyboard.js
@@ -0,0 +1,245 @@
+const Keyboard = {
+    elements: {
+        main: null,
+        keysContainer: null,
+        keys: []
+    },
+
+    shiftKeys: {
+        original: "1234567890,.?'[]",
+        shifted: "!~#$%^&*();:/\"{}"
+    },
+
+
+    foreign: {
+        tildeOrig: "naoNAO",
+        tildeShifted: [327, 227, 245, 
+            209, 195, 336],
+        aCute: "aeiouynAEIOUYN",
+        aCuteShifted: [225, 233, 237, 243, 250, 253, 241,
+            193, 201, 205, 211, 218, 221,209]
+    },
+
+    eventHandlers: {
+        oninput: null,
+        onclose: null
+    },
+
+    properties: {
+        value: "",
+        capsLock: false,
+        tilde: false,
+        acute: false,
+        currentKeyElement: null,
+        isOpen: false
+    },
+
+    init() {
+        // Create main elements
+        this.elements.main = document.createElement("div");
+        this.elements.keysContainer = document.createElement("div");
+
+        // Setup main elements
+        this.elements.main.classList.add("keyboard", "keyboard--hidden");
+        this.elements.keysContainer.classList.add("keyboard__keys");
+        this.elements.keysContainer.appendChild(this._createKeys());
+
+        this.elements.keys = this.elements.keysContainer.querySelectorAll(".keyboard__key");
+
+        // Add to DOM
+        this.elements.main.appendChild(this.elements.keysContainer);
+        document.body.appendChild(this.elements.main);
+
+        // Automatically use keyboard for elements with .use-keyboard-input
+        document.querySelectorAll(".use-keyboard-input").forEach(element => {
+            element.addEventListener("focus", () => {
+                this.open(element.value, currentValue => {
+                    element.value = currentValue;
+                });
+            });
+        });
+    },
+
+    _createKeys() {
+        const fragment = document.createDocumentFragment();
+        const keyLayout = [
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+            "'","q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+            "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+            "done", "z", "x", "c", "v", "b", "n", "m",",", ".", "?",
+            "@","[","]","space", "acute" 
+        ];
+
+        // Creates HTML for an icon
+        const createIconHTML = (icon_name) => {
+            return `<i class="material-icons">${icon_name}</i>`;
+        };
+
+
+        keyLayout.forEach(key => {
+            const keyElement = document.createElement("button");
+            const insertLineBreak = ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
+
+            // Add attributes/classes
+            keyElement.setAttribute("type", "button");
+            keyElement.classList.add("keyboard__key");
+
+            switch (key) {
+                case "backspace":
+                    keyElement.classList.add("keyboard__key--wide");
+                    keyElement.innerHTML = createIconHTML("backspace");
+
+                    keyElement.addEventListener("click", () => {
+                        this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
+                        this._triggerEvent("oninput");
+                    });
+
+                    break;
+
+
+                case "caps":
+                    keyElement.classList.add("keyboard__key--wide", "keyboard__key--activatable");
+                    keyElement.innerHTML = createIconHTML("keyboard_capslock");
+
+                    keyElement.addEventListener("click", () => {
+                        this._toggleCapsLock();
+                        keyElement.classList.toggle("keyboard__key--active", this.properties.capsLock);
+                    });
+
+                    break;
+
+                case "tilde":
+                    //setup toggeable button
+                    keyElement.classList.add("keyboard__key--activatable");
+                    keyElement.innerHTML = createIconHTML("circle");
+
+                    keyElement.addEventListener("click", () => {
+                        //this._toggleCapsLock();
+                        this.properties.tilde = !this.properties.tilde;
+                        keyElement.classList.toggle("keyboard__key--active", this.properties.tilde);
+                    });
+                    break;
+
+                case "acute":
+                    //setup toggeable button
+                    keyElement.classList.add("keyboard__key--activatable");
+                    keyElement.innerHTML = createIconHTML("circle");
+
+                    keyElement.addEventListener("click", () => {
+                        //this._toggleCapsLock();
+                        this.properties.acute = !this.properties.acute;
+                        keyElement.classList.toggle("keyboard__key--active", this.properties.acute);
+                    });
+                    break;
+
+
+                case "enter":
+                    keyElement.classList.add("keyboard__key--wide");
+                    keyElement.innerHTML = createIconHTML("keyboard_return");
+
+                    keyElement.addEventListener("click", () => {
+                        if (currentField == "userPassword")
+                            setCurrentField("pwdHint");
+                        else {
+                            this.properties.value += "\n";
+                            this._triggerEvent("oninput");
+                        }
+                    });
+
+                    break;
+
+                case "space":
+                    keyElement.classList.add("keyboard__key--extra-wide");
+                    keyElement.innerHTML = createIconHTML("space_bar");
+
+                    keyElement.addEventListener("click", () => {
+                        this.properties.value += " ";
+                        this._triggerEvent("oninput");
+                    });
+
+                    break;
+
+                case "done":
+                    keyElement.classList.add("keyboard__key--wide", "keyboard__key--dark");
+                    keyElement.innerHTML = createIconHTML("check_circle");
+
+                    keyElement.addEventListener("click", () => {
+                        this.close();
+                        this._triggerEvent("onclose");
+                    });
+
+                    break;
+
+                default:
+                    keyElement.textContent = key.toLowerCase();
+
+                    keyElement.addEventListener("click", () => {
+                        this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+                        this._triggerEvent("oninput");
+                    });
+
+                    break;
+            }
+
+            fragment.appendChild(keyElement);
+
+            if (insertLineBreak) {
+                fragment.appendChild(document.createElement("br"));
+            }
+        });
+
+        return fragment;
+    },
+
+    _triggerEvent(handlerName) {
+        if (typeof this.eventHandlers[handlerName] == "function") {
+            this.eventHandlers[handlerName](this.properties.value);
+        }
+    },
+
+    _toggleCapsLock() {
+
+        this.properties.capsLock = !this.properties.capsLock;
+
+        for (const key of this.elements.keys) {
+            if (key.childElementCount === 0) {
+                key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+
+                if (this.properties.capsLock) {
+                    if (this.shiftKeys.original.includes(key.textContent)) {
+                        let ix = this.shiftKeys.original.indexOf(key.textContent);
+                        key.textContent = this.shiftKeys.shifted.substr(ix, 1);
+                    }
+                }
+                else {
+                    if (this.shiftKeys.shifted.includes(key.textContent)) {
+                        let ix = this.shiftKeys.shifted.indexOf(key.textContent);
+                        key.textContent = this.shiftKeys.original.substr(ix, 1);
+                    }
+
+                }
+
+            }
+        }
+    },
+
+    open(initialValue, oninput, onclose) {
+        this.properties.value = initialValue || "";
+        this.eventHandlers.oninput = oninput;
+        this.eventHandlers.onclose = onclose;
+        this.elements.main.classList.remove("keyboard--hidden");
+        this.properties.isOpen = true;
+    },
+
+    close() {
+        this.properties.value = "";
+        this.eventHandlers.oninput = oninput;
+        this.eventHandlers.onclose = onclose;
+        this.elements.main.classList.add("keyboard--hidden");
+        this.properties.isOpen = false;
+    }
+};
+
+window.addEventListener("DOMContentLoaded", function () {
+    Keyboard.init();
+});
---
+++ Criptify2/Cryptico48_Deploy/2_Minified/filerender.js
@@ -0,0 +1,323 @@
+
+function showFileInfo() {
+	let msg = "";
+	let txtEncrypted = "";
+
+	hideControl("divMedia");
+	let isEncrypted = "";
+	if (encryptedFile) {
+		if (data.includes("Hint:")) {
+			let txHint = extract(data, "Hint:", "\n");
+			setField("pwdHint", txHint);
+		}
+		txtEncrypted = `<b class="text-warning">This is an encrypted file.</b>`;
+		isEncrypted = "Encrypted";
+	}
+
+	let fInfoRow = "";
+
+	let fSize = getSizeText(data.length);
+
+	if (usingFile && selFile.size > maxData) {
+		txtEncrypted = `<b class="text-danger">This file is ${fSize}, can not be processed with ${navigator.deviceMemory}GB RAM..</b>`;
+		showError(txtEncrypted);
+	}
+
+	let sDate = "";
+
+	if (usingFile)
+		sDate = `${selFile.lastModifiedDate}`;
+	else
+		sDate = `${new Date()}`;
+
+	let ix = sDate.indexOf("GMT");
+	if (ix > 0)
+		sDate = sDate.substring(0, ix).trim();
+
+	if (usingFile) {
+		if (isEncrypted.length > 0)
+			fInfoRow = `${folderIcon} <b>${selFile.name}</b> <i>${selFile.type}</i> ${sDate}, <b>${fSize}, <i class="red">${isEncrypted}</i>`;
+		else
+			fInfoRow = `${folderIcon} <b>${selFile.name}</b> <i>${selFile.type}</i> ${sDate}, <b>${fSize}`;
+
+	}
+	else {
+		let sourceText = "Manual Text.";
+		if (dataFromClipboard)
+			sourceText = "Clipbaord Text."
+
+		if (isEncrypted.length > 0)
+			fInfoRow = `${sourceText} (${data.length} bytes), <i class="red">${isEncrypted}</i>`;
+		else
+			fInfoRow = `${sourceText} (${data.length}bytes.)`;
+	}
+	showInfoAt("divFileInfo", fInfoRow);
+	showBlock("divFileInfo");
+	//showBlock("divMedia");
+	hideControl("divInputText");
+
+}
+
+function showPDF() {
+	// let pdfWindow = window.open("")
+	// pdfWindow.document.write(`<iframe width='100%' height='100%' src='${data}'></iframe>`);
+	iconHtml = `<i class="fas fa-file-pdf icon-size2" title="Open Binary File: Image,PDF,Excel,Word,Audio" id="clearText" style="margin-left: -30px; cursor: pointer;"
+	onclick="openPDF()"></i>
+	`;
+
+
+	isPDF = true;
+	renderPDF();
+	setField("loadedText", data);
+
+	if (usingFile && isGoogleVer) {
+		showBlock("divInputPDF");
+		hideControl("divDownload");
+	}
+	else {
+		hideControl("divInputPDF");
+		showDiv("divDownload");
+	}
+	mediaOpen = true;
+	if (mobile)
+		showPasswordMessage();
+
+}
+
+
+function clearMedia() {
+	showMessageAt("divFileInfo", "");
+	hideControl("divImage");
+	hideControl("divVideo");
+	hideControl("divAudio");
+	hideControl("divPDF");
+	hideControl("divText");
+	hideControl("divMedia");
+	mediaOpen = false;
+
+}
+function showVideo() {
+
+	currentMedia = "divVideo";
+	hideControl("divText");
+	if (data.length > 0) {
+		let html = `<video width="100%" height="auto" controls>
+	<source src="${data}" type="video/mp4">
+  Your browser does not support the video tag.
+  </video>`;
+		writeInnerHTML("divVideo", html);
+		showBlock("divVideo");
+		showBlock("divMedia");
+		showBlock("divView");
+		mediaOpen = true;
+		iconHtml = `<i class="fas fa-image icon-size2" title="Open Binary File: Image,PDF,Excel,Word,Audio" id="clearText" style="margin-left: -30px; cursor: pointer;"
+	onclick="toggleMedia()"></i>`;
+
+	}
+
+	function setHtml() {
+		return `			<audio class="form-control icon-size2" controls>
+	<source src="${data}" type="audio/ogg">
+	<source src="${data}" type="audio/mpeg">
+	Your browser does not support the audio element.
+	</audio>`;
+	}
+
+	function showAudio() {
+		//todo: disable playback 
+		hideControl("divText");
+		currentMedia = "divAudio";
+		if (data.length > 0) {
+			writeInnerHTML("divAudio", setHtml());
+			showBlock("divAudio");
+			showBlock("divMedia");
+			showBlock("divView");
+
+			mediaOpen = true;
+		}
+
+	}
+
+
+}
+
+function decodeUrlData() {
+	let decodedData = ""
+	encryptedFile = false;
+	if (data.includes(";base64,")) {
+		let parts = data.split(",");
+		if (parts.length == 2) {
+			decodedData = atob(parts[1]);
+			try {
+				let obj = JSON.parse(decodedData);
+				encryptedFile = (obj.data.length + obj.data2.length > 0);
+				//encryptedFile = decodedData.includes("data:") && decodedData.includes("data2:") && decodedData.includes("data3:");
+				data = decodedData;
+			}
+			catch (ex) {
+				data = decodedData;
+			}
+		}
+	}
+	else
+		encryptedFile = isEncryptedData();
+}
+
+
+function getFileTitle(fName) {
+	fileSizeText = getSizeText(data.length);
+	let ix = fName.lastIndexOf(".");
+	let ext = "";
+	if (ix > 0)
+		ext = fName.substr(ix);
+
+	let shortName = fileName.substr(0, ix).substr(0, 15);
+	if (fName.length > 15)
+		shortName += "...";
+
+	return shortName + " " + ext.toUpperCase() + ` (${fileSizeText})`;
+}
+
+function showData() {
+	encryptedFile = false;
+
+	hideControl("divImage");
+	hideControl("divVideo");
+	hideControl("divAudio");
+	hideControl("divPDF");
+	hideControl("divText");
+	hideControl("divInputPDF");
+	hideControl("divInfo");
+	hideControl("divHide");
+	hideControl("divSettings");
+	hideControl("divView");
+
+	mediaOpen = false;
+
+	canProcess = true;
+	let dataType = "text/plain";
+	if (selFile && usingFile) {
+		selFile.readSize = data.length;
+		canProcess = selFile.size <= maxData;
+		dataType = selFile.type;
+	}
+
+	if (dataType == "application/pdf")
+		showPDF();
+	else if (dataType.includes("image"))
+		showImage();
+	else if (dataType.includes("video"))
+		showVideo();
+	else if (dataType.includes("audio"))
+		showAudio();
+	else showText();
+	showFileInfo();
+	mediaOpen = true;
+	showBlock("divMedia");
+	showBlock("divHide");
+	toggleCanProcess(canProcess, fileSizeText);
+	if (encryptionDone || decryptionDone)
+		hideControl("divNext");
+	else
+		showBlock("divNext");
+
+	hideControl("divShare");
+}
+
+
+function showMedia(divId, hideResult = false) {
+	if (divId && divId.length == 0) {
+		hideControl("divText");
+		hideControl("divImage");
+		hideControl("divPDF");
+		hideControl("divVideo");
+		hideControl("divAudio");
+		currentMedia = "";
+	}
+
+	if (hideResult)
+		hideControl("result");
+
+	if (currentMedia.length > 0 && currentMedia != divId)
+		hideControl(currentMedia);
+
+	currentMedia = divId;
+	showBlock(divId);
+	showBlock("divMedia");
+	mediaOpen = true;
+}
+
+function showText() {
+	//todo: check for html, js and other text formats
+
+	currentMedia = "divText";
+	hideControl("divImage");
+	hideControl("divVideo");
+	hideControl("divAudio");
+	let fName = "Clipboard text";
+	decodeUrlData();
+	let ix = data.indexOf(`data:`);
+	if ( ix < 0 )
+		ix = data.indexOf(`"data":"`);
+	if (ix > 0)
+		setField("loadedText", data.substr(ix));
+	else
+		setField("loadedText", data);
+
+	if (data.includes("Hint:")) {
+		let txHint = extract(data, "Hint:", "\n");
+		setField("pwdHint", txHint);
+	}
+	showPasswordMessage();
+	showBlock("divText");
+	showBlock("divMedia");
+	showBlock("divHide");
+	mediaOpen = true;
+}
+
+function showNoVisor() {
+	//todo: check for html, js and other text formats
+	currentMedia = "divText";
+	let fName = "Clipboard text";
+	if (usingFile)
+		fName = getFileTitle(fileName);
+	decodeUrlData();
+
+
+	//setField("loadedText", "No visor for this type of file.");
+	setField("loadedText", data);
+	showBlock("divText");
+	showBlock("divMedia");
+	showBlock("divView");
+	showBlock("divHide");
+
+	mediaOpen = true;
+	if (encryptedFile) {
+		if (usingFile)
+			hideControl("divInputText");
+
+		if (data.includes("Hint:")) {
+			let txHint = extract(data, "Hint:", "\n");
+			setField("pwdHint", txHint);
+		}
+		//gotoPage(2);
+		//showWarning(`Encrypted Data loaded. Enter Password.`);
+	}
+}
+
+
+
+
+function showImage() {
+	currentMedia = "divImage";
+	hideControl("divText");
+	let image = document.getElementById("imgView");
+	try {
+		image.src = data;
+	}
+	catch (ex) {
+		showError("showImage() ex:" + ex.message);
+	}
+	showBlock("divImage");
+	mediaOpen = true;
+}
